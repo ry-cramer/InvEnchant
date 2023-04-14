960e7f240d51884d2226651b394b6628ac562386
Rebekah Cramer <80940081+reb-cramer@users.noreply.github.com>
2023-04-13 23:07:25 -0700

    First implementation of Replenish enchantment
    Major issues:
    1. NBT's all seem correct, but picking up dropped blocks or mining placed blocks doesn't fill up the inventory
    2. Every so often, the number of items in the selected slot decreases when an item is outputted. This item does update the inventory when picked up
    3. When the Replenish shulker box is placed, its inventory is reset to pre-replenish levels

--- InvEnchantMod/src/main/java/com/satiryz/invenchant/enchants/ReplenishEnchantment.java
+++ InvEnchantMod/src/main/java/com/satiryz/invenchant/enchants/ReplenishEnchantment.java
@@ -1,10 +1,13 @@
 package com.satiryz.invenchant.enchants;
 
 import com.satiryz.invenchant.InvEnchant;
+import com.satiryz.invenchant.capabilities.ModInventoryHandler;
+import com.satiryz.invenchant.capabilities.ModInventoryHandlerProvider;
 import com.satiryz.invenchant.init.EnchantmentInit;
 import com.satiryz.invenchant.tags.ShulkerLikeTag;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 
@@ -14,9 +17,45 @@ public class ReplenishEnchantment extends Enchantment {
 		super(Rarity.RARE, EnchantmentInit.SHULKER_LIKE, EquipmentSlot.values());
 		this.descriptionId = "enchantment." + InvEnchant.MODID + ".replenish";
 	}
-	
+
 	@Override
-	  public boolean canEnchant(ItemStack stack) {
-	    return ShulkerLikeTag.isShulkerLike(stack);
-	  }
+	public boolean canEnchant(ItemStack stack) {
+		return ShulkerLikeTag.isShulkerLike(stack);
+	}
+
+	public static void attemptRefill(Player player, ItemStack placedStack, int amountToFill, int slotPlacedFrom) {
+		ModInventoryHandler playerInventory = player.getCapability(ModInventoryHandlerProvider.INVENTORY_HANDLER)
+				.resolve().get();
+		for (ItemStack invStack : playerInventory.getStacks()) {
+			if (invStack.isEmpty()) {
+				continue;
+			}
+			// if item stack is a Replenish shulker box
+			if (invStack.getEnchantmentLevel(EnchantmentInit.REPLENISH_ENCHANT) > 0) {
+				ModInventoryHandler shulkerInventory = invStack
+						.getCapability(ModInventoryHandlerProvider.INVENTORY_HANDLER).resolve().get();
+				// for slot in shulker box
+				for (int shulkerSlot = 0; shulkerSlot < shulkerInventory.getSlots(); shulkerSlot++) {
+					// If placedStack matches shulker stack
+					if (isMatchingItem(placedStack, shulkerInventory.getStackInSlot(shulkerSlot))) {
+						// refill stack and return
+						refillStack(playerInventory, shulkerInventory, shulkerSlot, amountToFill, slotPlacedFrom);
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+	public static boolean isMatchingItem(ItemStack stackToRefill, ItemStack shulkerStack) {
+		return stackToRefill.getItem() == shulkerStack.getItem();
+	}
+	
+	public static void refillStack(ModInventoryHandler playerInventory, ModInventoryHandler shulkerInventory, int slotToTakeFrom, int amountToFill, int slotToFill) {
+		 System.out.println("Refill function reached");
+		 ItemStack refillStack = shulkerInventory.extractItem(slotToTakeFrom, amountToFill, false);
+		 
+		 playerInventory.insertItem(slotToFill, refillStack, false);
+		 System.out.println(playerInventory.serializeNBT());
+	}
 }
--- InvEnchantMod/src/main/java/com/satiryz/invenchant/events/ModEvents.java
+++ InvEnchantMod/src/main/java/com/satiryz/invenchant/events/ModEvents.java
@@ -3,9 +3,11 @@ package com.satiryz.invenchant.events;
 import com.satiryz.invenchant.InvEnchant;
 import com.satiryz.invenchant.capabilities.ModInventoryHandler;
 import com.satiryz.invenchant.capabilities.ModInventoryHandlerProvider;
+import com.satiryz.invenchant.enchants.ReplenishEnchantment;
 import com.satiryz.invenchant.init.EnchantmentInit;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -13,8 +15,10 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 @OnlyIn(Dist.CLIENT)
@@ -31,7 +35,7 @@ public class ModEvents {
 	@SubscribeEvent
 	public static void onAttachCapabilitiesContainer(AttachCapabilitiesEvent<ItemStack> event) {
 		ItemStack stack = event.getObject();
-		if (stack.getEnchantmentLevel(EnchantmentInit.SIPHON_ENCHANT) > 0) {
+		if (stack.getEnchantmentLevel(EnchantmentInit.SIPHON_ENCHANT) > 0 || stack.getEnchantmentLevel(EnchantmentInit.REPLENISH_ENCHANT) > 0) {
 			event.addCapability(new ResourceLocation(InvEnchant.MODID, "properties.shulker_box"),
 					new ModInventoryHandlerProvider(stack));
 		}
@@ -53,5 +57,40 @@ public class ModEvents {
 	@SubscribeEvent
 	public static void registerCaps(RegisterCapabilitiesEvent event) {
 		event.register(ModInventoryHandler.class);
+	}
+	
+	private static final PreviousInventory prevInv = new PreviousInventory();
+
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent event) {
+		if (event.side != LogicalSide.CLIENT || event.player.isCreative()) {
+			return;
+		}
+		
+		Player player = event.player;
+		ItemStack itemStackMainHand = player.getItemInHand(InteractionHand.MAIN_HAND);
+		ItemStack itemStackOffHand = player.getItemInHand(InteractionHand.OFF_HAND);
+		int selectedSlot = player.getInventory().selected;
+		
+		if (selectedSlot == prevInv.selectedSlot){
+			// if the  stack size is less than the previous stack size and the main/off hand haven't been switched
+			if (itemStackMainHand.getCount() < prevInv.itemStackMainHand.getCount()
+					&& (!ItemStack.matches(itemStackMainHand, prevInv.itemStackOffHand) || ItemStack.matches(itemStackMainHand, itemStackOffHand))) {
+				System.out.println("Item dropped/placed from main hand");
+				ReplenishEnchantment.attemptRefill(player, prevInv.itemStackMainHand, prevInv.itemStackMainHand.getCount() - itemStackMainHand.getCount(), selectedSlot);
+			} else if (itemStackOffHand.getCount() < prevInv.itemStackOffHand.getCount()
+					&& (!ItemStack.matches(itemStackOffHand, prevInv.itemStackMainHand) || ItemStack.matches(itemStackMainHand, itemStackOffHand))) {
+				System.out.println("Item dropped/placed from offhand");
+			}
+		}
+		prevInv.itemStackMainHand = itemStackMainHand.copy();
+		prevInv.itemStackOffHand = itemStackOffHand.copy();
+		prevInv.selectedSlot = selectedSlot;
+	}
+	
+	private static class PreviousInventory {
+		ItemStack itemStackMainHand = ItemStack.EMPTY;
+		ItemStack itemStackOffHand = ItemStack.EMPTY;
+		int selectedSlot = -1;
 	}
 }
